858807a94b77c0e07686814841a8c79ec1031e20
Koval <dmytrokovalskyi@yahoo.com>
2020-09-29 01:55:25 +0300

    one more bug

---
+++ build/precache-manifest.7aa45889e983fde7a82de118cc7126c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6323b3247c1b3263d003db7ebc4712e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c64257903ee53346f10b",
+    "url": "/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "0fcd95e229f5a2bfb579",
+    "url": "/static/css/main.e1d168a2.chunk.css"
+  },
+  {
+    "revision": "c64257903ee53346f10b",
+    "url": "/static/js/2.be1edf58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.be1edf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b103a0898fa3ddbb6a7a",
+    "url": "/static/js/3.3c31a260.chunk.js"
+  },
+  {
+    "revision": "0fcd95e229f5a2bfb579",
+    "url": "/static/js/main.ff5ac72e.chunk.js"
+  },
+  {
+    "revision": "5f23578fc76bfd75a034",
+    "url": "/static/js/runtime-main.756b03bb.js"
+  }
+]);
